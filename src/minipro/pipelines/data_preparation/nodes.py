b5d3e0b9583a52cb15fb81ae36811802018ab948
qcoopman <quentin.coopman@gmail.com>
2021-12-06 19:40:11 -0500

    Add comments on variables & new thresholds

--- src/minipro/pipelines/data_preparation/nodes.py
+++ src/minipro/pipelines/data_preparation/nodes.py
@@ -8,7 +8,7 @@ def preprocess(data_dir: str) -> pd.DataFrame:
     """
     Reads all data files found in an input DATA_DIR and filters out malformed
     data points. Each file represents all satellite measurements of clouds in
-    a given day over the Southern Ocean from 2005 to 2017. Each data point has
+    a given day over the Southern Ocean from 2005 to 2017. Each data or line represents one cloud object (Connected cloudy pixels from CLAAS-2 dataset,  https://doi.org/10.5676/EUM_SAF_CM/CLAAS/V002). Each data point has
     28 columns
     Args:
         data_dir: Folder that contains all data files
@@ -24,27 +24,27 @@ def preprocess(data_dir: str) -> pd.DataFrame:
         "re",                   # Hydrometeor effective radius
         "std_re",               # Std re
         "ctt",                  # Cloud top temperature
-        "std_ctt",              #
-        "cth_mp",
-        "std_cth",
-        "perim",
-        "nb_ice",
-        "nb_liq",
-        "re_liq",
-        "re_ice",
+        "std_ctt",              # Standad deviation ctt
+        "cth_mp",               # Cloud top height
+        "std_cth",              # Std CTH
+        "perim",                # Cloud perimeter
+        "nb_ice",               # Number of ice pixels
+        "nb_liq",               # Number of liquid pixels
+        "re_liq",               # Mean effective radius of liquid cloud droplets
+        "re_ice",               # Mean effective radius of ice crytals
         "off",
-        "nb_pocket_ice",
-        "size_pocket_ice",
-        "size_pocket_std_ice",
-        "nb_pocket_liq",
-        "size_pocket_liq",
-        "size_pocket_std_liq",
-        "tau_liq",
-        "tau_ice",
-        "lon",
-        "lat",
-        "min_ctt",
-        "max_ctt",
+        "nb_pocket_ice",        # Number of ice pockets (cluster of ice pixels) within the cloud
+        "size_pocket_ice",      # Mean size of ice pockets
+        "size_pocket_std_ice",  # Standard deviation of ice pocket size
+        "nb_pocket_liq",        # Number of liquid pokets (cluster of liquid pocket) within the cloud
+        "size_pocket_liq",      # Mean size of liquid pockets
+        "size_pocket_std_liq",  # Standard deviation of liquid pocket size
+        "tau_liq",              # Mean optical thickness of liquid pixels
+        "tau_ice",              # Mean optical thickness of ice pixels
+        "lon",                  # Mean longitude of cloud object
+        "lat",                  # Mean latitude of cloud object
+        "min_ctt",              # Minimum of cloud top temperature
+        "max_ctt",              # Maximum of cloud top temperature
     ]
 
     # Retrieve a list of the paths of all data files
@@ -59,8 +59,8 @@ def preprocess(data_dir: str) -> pd.DataFrame:
         df = df[
             ~df["re_liq"].isna()
             & ~df["re_ice"].isna()
-            & (df["nb_ice"] != 0.0)
-            & (df["nb_liq"] != 0.0)
+            & (df["nb_ice"] > 3.0)
+            & (df["nb_liq"] > 3.0)
             & (df["area"] > 50)
             & (df["tau"] > 1.0)
             & (df["size_pocket_ice"] != df["area"])
